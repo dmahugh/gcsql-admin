4a710f0be074b7f9ae950efb09078d833f285946
Doug Mahugh <dmahugh@microsoft.com>
2019-05-17 09:57:33 +0200

    update config file

---
+++ config.py
@@ -0,0 +1,10 @@
+"""configuration settings
+"""
+
+MY_PROJECT = "YOUR_PROJECT_NAME"
+MY_INSTANCE = "YOUR_INSTANCE_NAME"
+
+# If you've created a service account (and you should!), edit this
+# setting to the name of the app credentials JSON file for your
+# application. Note that the path is relative to the project folder.
+APP_CREDENTIALS = ""
